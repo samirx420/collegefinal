dbbb8770fe09d6c921db06cd908904c5f7967744
Samir <syambob666@gmail.com>
2020-09-30 18:13:46 +0545

    stacked charts done

--- src/App.js
+++ src/App.js
@@ -1,47 +1,76 @@
 import React, { useState } from "react";
 import "./App.css";
-import PieChart from "./PieChart";
+import StackedBarChart from "./StackedBarChart";
+
+const data = [
+  {
+    year: 1980,
+    "🥑": 10,
+    "🍌": 20,
+    "🍆": 30
+  },
+  {
+    year: 1990,
+    "🥑": 20,
+    "🍌": 40,
+    "🍆": 60
+  },
+  {
+    year: 2000,
+    "🥑": 30,
+    "🍌": 45,
+    "🍆": 80
+  },
+  {
+    year: 2010,
+    "🥑": 40,
+    "🍌": 60,
+    "🍆": 100
+  },
+  {
+    year: 2020,
+    "🥑": 50,
+    "🍌": 80,
+    "🍆": 120
+  }
+];
+
+const allKeys = ["🥑", "🍌", "🍆"];
+
+const colors = {
+  "🥑": "green",
+  "🍌": "orange",
+  "🍆": "purple"
+};
 
 function App() {
-  const [data, setData] = useState([
-    {
-      name: "apples",
-      value: 100
-    },
-    {
-      name: "bananas",
-      value: 300
-    },
-    {
-      name: "cherries",
-      value: 250
-    }
-  ]);
-
+  const [keys, setKeys] = useState(allKeys);
   return (
     <React.Fragment>
-      <h1>Pie Chart</h1>
-      <PieChart data={data} />
-      <button
-        onClick={() => {
-          setData([
-            {
-              name: "apples",
-              value: Math.round(Math.random() * 100)
-            },
-            {
-              name: "bananas",
-              value: Math.round(Math.random() * 200)
-            },
-            {
-              name: "cherries",
-              value: Math.round(Math.random() * 300)
-            }
-          ]);
-        }}
-      >
-        Randomize
-      </button>
+      <h2>Stacked Bar Chart with D3 </h2>
+      <StackedBarChart data={data} keys={keys} colors={colors} />
+
+      <div className="fields">
+        {allKeys.map(key => (
+          <div key={key} className="field">
+            <input
+              id={key}
+              type="checkbox"
+              checked={keys.includes(key)}
+              onChange={e => {
+                if (e.target.checked) {
+                  setKeys(Array.from(new Set([...keys, key])));
+                } else {
+                  setKeys(keys.filter(_key => _key !== key));
+                }
+              }}
+            />
+            <label htmlFor={key} style={{ color: colors[key] }}>
+              {key}
+            </label>
+          </div>
+        ))}
+      </div>
     </React.Fragment>
   );
 }
